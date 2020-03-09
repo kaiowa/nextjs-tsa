31ea32f2cfcb29f3eed51dbde8fe8ddf504a8c6c
Kaiowa <kaiowa@gmail.com>
2020-03-09 16:12:14 +0100

    categories

--- front/components/boxcategory/boxcategory.js
+++ front/components/boxcategory/boxcategory.js
@@ -1,5 +1,6 @@
 import boxStyles from "./boxcategory.module.scss";
 import Link from 'next/link';
+import CategoriaName from "../../pages/categoria/[id]/[name]";
 const BoxCategory = props =>{
 
   console.log('props boxcategory',props);
@@ -9,13 +10,19 @@ function getLink(id,name){
 return (
   <div className={boxStyles.boxCategory}>
       <div className={boxStyles.shopcatbox}>
-      
+      <Link  href={{ pathname: `/categoria/${props.category.id}/${props.category.slug}.html` }} >
         <img src={'/images/categories/'+props.category.image} title={props.category.name+' running'}/>
+        </Link>
         <h3 className={boxStyles.name}>{props.category.name}</h3>
+  
         <Link  href={{ pathname: `/categoria/${props.category.id}/${props.category.slug}.html` }} >
           <button className={boxStyles.button}>Ver mas</button>
         </Link>
+  
       
+      </div>
+      <div className={boxStyles.description}>
+      {props.category.description}
       </div>
   </div>
 )}
